29245bb95d30fa89f8002899b6d1a42a0c6d7302
tobi <tobias@h3rrmann.ch>
2013-11-05 20:15:59 +0100

    Testcode im index.php

--- w9/index.php
+++ w9/index.php
@@ -5,3 +5,7 @@ require_once "./common/Response.php";
 require_once "./common/HttpRequestImpl.php";
 require_once "./common/HttpResponseImpl.php";
 
+$request = new HttpRequestImpl();
+$response = new HttpResponseImpl();
+
+echo $request->getParameters();
