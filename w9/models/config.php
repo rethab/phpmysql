864459e67641f83818d128e631d9c411ec37ff81
mih <minimih@gmail.com>
2013-11-05 20:16:13 +0100

    dies und das

--- w9/models/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-date_default_timezone_set('Europe/Zurich');
-
-define( "DATABASE_NAME", "loc_orm" );
-define( "DATABASE_HOST", "localhost" );
-define( "DATABASE_USER", "loc_orm" );
-define( "DATABASE_PASSWORD", "12341234" );
-?>
