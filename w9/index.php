91e513821623a89393f75d091a51bbdcfdbd4c28
Reto Hablützel <rethab@rethab.ch>
2013-11-05 20:13:36 +0100

    flash import in index

--- w9/index.php
+++ w9/index.php
@@ -4,4 +4,5 @@ require_once "./common/Request.php";
 require_once "./common/Response.php";
 require_once "./common/HttpRequestImpl.php";
 require_once "./common/HttpResponseImpl.php";
+require_once "./helper/Flash.php";
 
