92866b7fc63ea7cb837754d195704f2d4ebc7adb
Jan <jan@Deuterium.local>
2013-11-05 19:27:44 +0100

    post model added

---
+++ w9/models/config.php
@@ -0,0 +1,9 @@
+<?php
+
+date_default_timezone_set('Europe/Zurich');
+
+define( "DATABASE_NAME", "loc_orm" );
+define( "DATABASE_HOST", "localhost" );
+define( "DATABASE_USER", "loc_orm" );
+define( "DATABASE_PASSWORD", "12341234" );
+?>
